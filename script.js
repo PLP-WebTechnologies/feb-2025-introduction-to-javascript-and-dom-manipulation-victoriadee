18e23f2539408c4550be7607ffd222f4047eb90d
victoriadee <victoriadatieno@gmail.com>
2025-05-21 22:31:40 +0300

    changes to script

--- script.js
+++ script.js
@@ -1,2 +1,13 @@
 var name = "Instructor Eddy";
 console.log(name); //output: Instructor Eddy
+
+//Let keyword = Value;
+let age = 30;
+console.log(age); //output 30
+
+age = 50; //re-assigning the value of age
+console.log(age);
+name = "Instructor John"; 
+console.log(name); 
+
+
